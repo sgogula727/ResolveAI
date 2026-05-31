75733ed38258dd87998a576cac76c73fcd0603cd
Sai Maanasa Gogula <saimaanasagogula@mac.mynetworksettings.com>
2026-05-31 11:07:11 -0400

    Remove tracked frontend build artifacts and keep .next ignored

--- frontend/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/tickets":["static\u002Fchunks\u002Fpages\u002Ftickets.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Ftickets"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
